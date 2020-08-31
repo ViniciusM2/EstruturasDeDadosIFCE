b2b10505ef0d16e2648a38bd343f7d62fcd5a58d
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2020-08-30 21:29:53 -0300

    forth commit

--- src/tarefa/Teste.java
+++ src/tarefa/Teste.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class Teste {
 	
 	Scanner scan = new Scanner(System.in);
+	
+	void inserirNaAgenda() {
+		
+	}
+	
+	void consultarAgenda() {
+		
+	}
+	
+	void excluirDaAgenda() {
+		
+	}
+	
+	void listarAgenda() {
+		
+	}
+	
+	void sair() {
+		
+	}
 
 	public static void main(String[] args) {
 		Agenda[] agenda = new Agenda[10];
@@ -16,15 +36,37 @@ public class Teste {
 			System.out.println("4 — Listar a agenda");
 			System.out.println("0 — Sair");
 			
-			System.out.println("Digite um número");
+			System.out.println("Digite o dígito de uma opção e então ENTER");
 			
 			Scanner scan = new Scanner(System.in);
 			
-			System.out.println(scan.next());
+			switch (scan.nextInt()) {
+			case 1:
+				System.out.println("Opção 1!");
+				break;
+			case 2:
+				System.out.println("Opção 2!");
+				break;
+			case 3:
+				System.out.println("Opção 3!");
+				break;
+			case 4:
+				System.out.println("Opção 4!");
+				break;
+			case 0:
+				System.out.println("Obrigado por sair!");
+			default:
+				System.out.println("Por favor, insira algo válido!");
+				break;
+			}
 			
 			
 			
 		}
+		
+		
+		
+		
 
 	}
 
