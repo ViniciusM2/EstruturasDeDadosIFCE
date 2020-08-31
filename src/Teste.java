e908f38a55aa3b0dc920d2149fb0a0c93270c606
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2020-08-30 21:03:07 -0300

    second commit

---
+++ src/Teste.java
@@ -0,0 +1,25 @@
+
+import tarefa.Agenda;
+import java.util.Scanner;
+
+public class Teste {
+	
+	Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Agenda[] agenda = new Agenda[10];
+		
+		while(true) {
+			System.out.println("1 — Inserir na agenda");
+			System.out.println("2 — Consultar agenda");
+			System.out.println("3 — Excluir da agenda");
+			System.out.println("4 — Listar a agenda");
+			System.out.println("0 — Sair");
+			
+			
+			
+		}
+
+	}
+
+}
